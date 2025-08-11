5d67b27ac3d425013b667be21615c638fc57feaa
zenin5479 <zenin5479@gmail.com>
2025-08-11 16:58:40 +0300

    Update 0.8 Обновлен метод. Проведен рефакторинг

---
+++ Structure_03/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Structure_03
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
