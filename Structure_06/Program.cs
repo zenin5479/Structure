941c5ef033d152ea47ebc61798f784a1b4411905
zenin5479 <zenin5479@gmail.com>
2025-08-11 19:56:08 +0300

    Update 1.6 Обновлен метод. Проведен рефакторинг

--- Structure_06/Program.cs
+++ Structure_06/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
+// Структуры
+// Статический конструктор
+
 namespace Structure_06
 {
-   internal class Program
+   struct MyStruct
    {
-      static void Main(string[] args)
+      public int Field;
+
+      // Статический конструктор всегда отрабатывает первым
+      static MyStruct()
       {
-         Console.WriteLine("Hello World!");
+         Console.WriteLine("Static Constructor");
+      }
+
+      // Если в структуре имеется пользовательский конструктор, то требуется в нем инициализировать все поля
+      public MyStruct(int value)
+      {
+         Console.WriteLine("Constructor");
+         this.Field = value;
+      }
+   }
+
+   class Program
+   {
+      static void Main()
+      {
+         // Создание экземпляра структурного типа, с вызовом пользовательского конструктора
+         MyStruct instance = new MyStruct { Field = 0 };
+
+         Console.WriteLine(instance.Field);
+
+         // Delay
+         Console.ReadKey();
       }
    }
 }
