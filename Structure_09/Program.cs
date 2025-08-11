8a854285c389b829609c7443a30fa84b0ebebcd8
zenin5479 <zenin5479@gmail.com>
2025-08-11 20:03:14 +0300

    Update 2.0 Обновлен метод. Проведен рефакторинг

--- Structure_09/Program.cs
+++ Structure_09/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 
+// Структуры
+// Пример плохого дизайна
+
 namespace Structure_09
 {
-   internal class Program
+   struct ZipCode
    {
-      static void Main(string[] args)
+      // Поля
+      int _fiveDigitCode;
+      int _plusFourExtension;
+
+      // Свойства
+      public int FiveDigitCode
       {
-         Console.WriteLine("Hello World!");
+         get { return _fiveDigitCode; }
+         set { _fiveDigitCode = value; }
+      }
+
+      public int PlusFourExtension
+      {
+         get { return _plusFourExtension; }
+         set { _plusFourExtension = value; }
+      }
+   }
+
+   class Program
+   {
+      static void Main()
+      {
+         ZipCode zipCode = new ZipCode();
+
+         zipCode.FiveDigitCode = 12345;
+         zipCode.PlusFourExtension = 1234;
+
+         Console.WriteLine(zipCode.FiveDigitCode);
+         Console.WriteLine(zipCode.PlusFourExtension);
+
+         // Delay
+         Console.ReadKey();
       }
    }
 }
