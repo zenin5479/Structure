9ad98c1adb4e1c90391ef2d161162e6798e2a822
zenin5479 <zenin5479@gmail.com>
2025-08-11 16:54:14 +0300

    Update 0.5 Обновлен метод. Проведен рефакторинг

--- Structure_02/Program.cs
+++ Structure_02/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 
-namespace Structure_02
+// Структуры.
+
+// Если в структуре имеются члены, которые обращаются к полю и нет пользовательского конструктора,
+//  то требуется при создании экземпляра вызывать конструктор по умолчанию. (Иначе будет ошибка.)
+
+namespace Structure
 {
-   internal class Program
+   struct MyStruct
    {
-      static void Main(string[] args)
+      private int field;
+
+      public int Field
       {
-         Console.WriteLine("Hello World");
+         get { return field; }
+         set { field = value; }
+      }
+
+      public void Show()
+      {
+         Console.WriteLine(field);
+      }
+   }
+
+   class Program
+   {
+      static void Main()
+      {
+         // Создание экземпляра структурного типа с вызовом конструктора по умолчанию.
+         MyStruct instance = new MyStruct();
+
+         instance.Field = 1;
+
+         Console.WriteLine(instance.Field);
+
+         // Delay.
+         Console.ReadKey();
       }
    }
 }
