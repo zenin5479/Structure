3277e657579484f96cb3facd3d844487b12dc2aa
zenin5479 <zenin5479@gmail.com>
2025-08-11 20:01:14 +0300

    Update 1.9 Обновлен метод. Проведен рефакторинг

--- Structure_08/Program.cs
+++ Structure_08/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
+// Структуры
+// В структурах можно создавать автоматически реализуемые свойства,
+// при этом требуется использовать конструктор при построении экземпляра
+
 namespace Structure_08
 {
-   internal class Program
+   struct MyStruct
    {
-      static void Main(string[] args)
+      public int MyProperty { get; set; }
+   }
+
+   class Program
+   {
+      static void Main()
       {
-         Console.WriteLine("Hello World!");
+         MyStruct instance = new MyStruct();
+
+         instance.MyProperty = 1;
+         Console.WriteLine(instance.MyProperty);
+
+         // Delay
+         Console.ReadKey();
       }
    }
 }
