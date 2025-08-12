dbbe84f9af97e8b145e070c84ea5a377bbf63cd4
zenin5479 <zenin5479@gmail.com>
2025-08-12 08:55:59 +0300

    Update 4.1 Обновлен метод. Проведен рефакторинг

--- Structure_11/Notebook.cs
+++ Structure_11/Notebook.cs
@@ -6,24 +6,9 @@ namespace Structure_11
    {
       readonly string _producer;
       readonly double _price;
+      private readonly string Model;
 
-      // Свойство только для чтения поля model
-      public string Model
-      {
-         get;
-      }
-
-      // Свойство только для чтения поля producer
-      public string Producer
-      {
-         get { return _producer; }
-      }
-
-      // Свойство только для чтения поля price
-      public double Price
-      {
-         get { return _price; }
-      }
+      
 
       // Пользовательский конструктор, принимает 3 аргумента
       public Notebook(string model, string producer, double price)
